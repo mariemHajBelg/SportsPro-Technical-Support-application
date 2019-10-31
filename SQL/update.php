5a8da896d66928c0de0c0b23f827fa77368dbe11
Mariem Haj Belgacem <49621150+mariemHajBelg@users.noreply.github.com>
2019-10-31 14:49:01 -0400

    Update update.php

--- SQL/update.php
+++ SQL/update.php
@@ -1,17 +1,28 @@
 <?php
 function update($con,$table,$field,$new_value, $key,$key_value){
-    echo "<br>Update<br>";
-/*    try{
+    try{
         openConnection($con);
         
-        $stmt = "UPDATE $table SET $field = '$new_value' WHERE $key = '$key_value'";
+        $stmt = "";
         
-        mysqli_stmt_execute($stmt) or die("Execution Error: ".mysqli_errno($con));
-        closeConnection();
+        if(gettype($field)!="array") $stmt = "UPDATE $table SET $field = '$new_value' WHERE $key = '$key_value'";
+        else{
+            $new_fields = "";
+            for($i=0;$i<sizeof($field);$i++) {
+                $tokenizer = ", ";
+                if($i==(sizeof($field)-1)) $tokenizer = "";
+                
+                $new_fields = $new_fields.$field[$i]."='".$new_value[$i]."'".$tokenizer;
+            }
+            $stmt = "UPDATE ".$table." SET ".$new_fields." WHERE ".$key."='".$key_value."';";
+        }
+        
+        mysqli_query($con,$stmt) or die("Execution Error: ".mysqli_errno($con));
+        return true;
     } catch (Exception $e){
         exit("Input Error.");
     }
-*/   
+  
 }
 
 
